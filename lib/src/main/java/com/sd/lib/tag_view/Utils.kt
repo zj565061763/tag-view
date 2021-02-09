c39a8eb524635a3753b373b41d56743729f2e7b9
zjun <565061763@qq.com>
2021-02-09 14:37:16 +0800

    Utils kotlin

--- lib/src/main/java/com/sd/lib/tag_view/Utils.kt
+++ lib/src/main/java/com/sd/lib/tag_view/Utils.kt
@@ -3,19 +3,21 @@ package com.sd.lib.tag_view
 import android.os.Build
 import android.view.View
 
-internal class Utils {
-    companion object {
-        @JvmStatic
-        fun isAttached(view: View?): Boolean {
-            if (view == null) return false
-            return if (Build.VERSION.SDK_INT >= 19) view.isAttachedToWindow else view.windowToken != null
-        }
+internal object Utils {
 
-        @JvmStatic
-        fun getObjectId(obj: Any): String {
-            val className = obj.javaClass.name
-            val identityHashCode = System.identityHashCode(obj)
-            return className + "@" + Integer.toHexString(identityHashCode)
-        }
+    @JvmStatic
+    fun isAttached(view: View?): Boolean {
+        if (view == null) return false
+        return if (Build.VERSION.SDK_INT >= 19)
+            view.isAttachedToWindow
+        else
+            view.windowToken != null
+    }
+
+    @JvmStatic
+    fun getObjectId(obj: Any): String {
+        val className = obj.javaClass.name
+        val identityHashCode = System.identityHashCode(obj)
+        return "${className}@${Integer.toHexString(identityHashCode)}"
     }
 }
