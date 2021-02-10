a9daa52b62f67a4d9c9e2cbbf0925c43c859bda0
zjun <565061763@qq.com>
2021-02-11 00:21:20 +0800

    kotlin

--- app/src/main/java/com/sd/demo/tag_view/item/TestItem.kt
+++ app/src/main/java/com/sd/demo/tag_view/item/TestItem.kt
@@ -6,10 +6,10 @@ import com.sd.lib.tag_view.ITagView
 class TestItem : ITagView.Item {
 
     override fun init(tagView: ITagView) {
-        Log.i(TestItem::class.simpleName, "init:$tagView $this")
+        Log.i(TestItem::class.java.simpleName, "init:$tagView $this")
     }
 
     override fun destroy() {
-        Log.i(TestItem::class.simpleName, "destroy $this")
+        Log.i(TestItem::class.java.simpleName, "destroy $this")
     }
 }
