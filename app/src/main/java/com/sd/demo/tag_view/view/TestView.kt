45b0699cbbbcfe96624290ee97fc4d3b3ead1691
zjun <565061763@qq.com>
2021-02-19 10:39:27 +0800

    demo

--- app/src/main/java/com/sd/demo/tag_view/view/TestView.kt
+++ app/src/main/java/com/sd/demo/tag_view/view/TestView.kt
@@ -8,10 +8,16 @@ import com.sd.demo.tag_view.item.TestItem
 import com.sd.demo.tag_view.view.TestView
 import com.sd.lib.tag_view.FTagViewApi
 
-class TestView(context: Context, attrs: AttributeSet?) : FrameLayout(context, attrs) {
+class TestView : FrameLayout {
     private val mTagViewApi = FTagViewApi(this@TestView)
 
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        Log.i(TestView::class.java.simpleName, "secondary constructor instance:${this}")
+    }
+
     init {
+        Log.i(TestView::class.java.simpleName, "init block context:${context} instance:${this}")
+
         mTagViewApi.prepare(object : FTagViewApi.PrepareCallback {
             override fun onPrepared(tagViewApi: FTagViewApi) {
                 Log.i(TestView::class.java.simpleName, "FTagViewApi getViewTag:${tagViewApi.viewTag} getItem:${tagViewApi.getItem(TestItem::class.java)}")
