33386c8046fc369fa9dc0e9a8504e6e14ea9ba9d
zjun <565061763@qq.com>
2021-02-20 14:22:11 +0800

    tag

--- app/src/main/java/com/sd/demo/tag_view/MainActivity.kt
+++ app/src/main/java/com/sd/demo/tag_view/MainActivity.kt
@@ -6,6 +6,8 @@ import com.sd.demo.tag_view.databinding.ActivityMainBinding
 import com.sd.lib.tag_view.FTagViewManager
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = MainActivity::class.java.simpleName
+
     private lateinit var mBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
