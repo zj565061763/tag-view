8c2f01040d536c13e1259f168ab07c6ad5b91b5b
zjun <565061763@qq.com>
2021-02-22 15:48:08 +0800

    demo

--- app/src/main/java/com/sd/demo/tag_view/MainActivity.kt
+++ app/src/main/java/com/sd/demo/tag_view/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.sd.demo.tag_view
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.sd.demo.tag_view.databinding.ActivityMainBinding
 import com.sd.lib.tag_view.FTagViewManager
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private val TAG = MainActivity::class.java.simpleName
 
     private lateinit var mBinding: ActivityMainBinding
@@ -15,6 +16,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
+    }
+
+    override fun onClick(v: View?) {
+
     }
 
     override fun onDestroy() {
