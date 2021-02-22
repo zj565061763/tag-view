a0a21e5499b0403de89f9fee7e2ffa826712193f
zjun <565061763@qq.com>
2021-02-22 15:50:16 +0800

    demo

--- app/src/main/java/com/sd/demo/tag_view/MainActivity.kt
+++ app/src/main/java/com/sd/demo/tag_view/MainActivity.kt
@@ -5,11 +5,14 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.sd.demo.tag_view.databinding.ActivityMainBinding
 import com.sd.lib.tag_view.FTagViewManager
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private val TAG = MainActivity::class.java.simpleName
 
     private lateinit var mBinding: ActivityMainBinding
+    private val mMainScope = MainScope()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         FTagViewManager.default.mIsDebug = true;
@@ -25,5 +28,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onDestroy() {
         super.onDestroy()
         mBinding.viewTag.itemManager.destroyItem()
+        mMainScope.cancel()
     }
 }
