230fa3722bbffdc0fc11c3cc2f660911b9b9b7e5
zjun <565061763@qq.com>
2021-05-11 15:12:28 +0800

    ITagView.findTag

--- lib/src/main/java/com/sd/lib/tag_view/ITagView.kt
+++ lib/src/main/java/com/sd/lib/tag_view/ITagView.kt
@@ -21,5 +21,13 @@ interface ITagView {
             val clazz = T::class.java
             return FTagViewManager.findTagView(clazz, view)
         }
+
+        /**
+         * 查找[view]所依附的[clazz]的[viewTag]
+         */
+        inline fun <reified T : ITagView> findTag(view: View): String? {
+            val tagView = find<T>(view)
+            return if (tagView != null) tagView.viewTag else EMPTY_VIEW_TAG
+        }
     }
 }
